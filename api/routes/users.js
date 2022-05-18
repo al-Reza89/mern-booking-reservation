b352999f62fec9f17a565ccfcdeb906d42d451c5
Reza <alialreza98@gmail.com>
2022-05-18 23:48:00 +0600

    jwt authentication for admin and users

--- api/routes/users.js
+++ api/routes/users.js
@@ -5,25 +5,32 @@ import {
   getUsers,
   updateUser,
 } from "../controllers/user.js";
-import { verifyToken } from "../utils/verifyToken.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-router.get("/checkauthentication", verifyToken, (req, res, next) => {
-  res.send("hello user , you are logged in");
-});
+// router.get("/checkauthentication", verifyToken, (req, res, next) => {
+//   res.send("hello user , you are logged in");
+// });
+
+// router.get("/checkuser/:id", verifyUser, (req, res, next) => {
+//   res.send("hello user , you are logged in and you can delete your account ");
+// });
+// router.get("/checkadmin/:id", verifyAdmin, (req, res, next) => {
+//   res.send("hello user , you are logged in and you can delete all account ");
+// });
 
 // update
-router.put("/:id", updateUser);
+router.put("/:id", verifyUser, updateUser);
 
 // delete
-router.delete("/:id", deleteUser);
+router.delete("/:id", verifyUser, deleteUser);
 
 // get
 
-router.get("/:id", getUser);
+router.get("/:id", verifyUser, getUser);
 
 // get all
-router.get("/", getUsers);
+router.get("/", verifyAdmin, getUsers);
 
 export default router;
