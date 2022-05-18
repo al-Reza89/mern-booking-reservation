da76c446bf654c2817793fbbde239a62607f8970
Reza <alialreza98@gmail.com>
2022-05-18 23:23:35 +0600

    jwt awenthecate and verify

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,4 +1,29 @@
 import express from "express";
+import {
+  deleteUser,
+  getUser,
+  getUsers,
+  updateUser,
+} from "../controllers/user.js";
+import { verifyToken } from "../utils/verifyToken.js";
+
 const router = express.Router();
+
+router.get("/checkauthentication", verifyToken, (req, res, next) => {
+  res.send("hello user , you are logged in");
+});
+
+// update
+router.put("/:id", updateUser);
+
+// delete
+router.delete("/:id", deleteUser);
+
+// get
+
+router.get("/:id", getUser);
+
+// get all
+router.get("/", getUsers);
 
 export default router;
