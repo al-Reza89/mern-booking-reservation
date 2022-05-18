6ce7423edc0888098cb3b9b23117d078888ab23c
Reza <alialreza98@gmail.com>
2022-05-18 20:25:26 +0600

    password hashing authentication complete

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { register } from "../controllers/auth.js";
+import { login, register } from "../controllers/auth.js";
 const router = express.Router();
 
 // localhost:8800/api/v1/register
 router.post("/register", register);
+router.post("/login", login);
 
 export default router;
