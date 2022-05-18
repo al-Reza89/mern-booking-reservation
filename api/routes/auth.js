6cc7b9d903162e5705cfe4655bc410834a48ca73
Reza <alialreza98@gmail.com>
2022-05-18 19:55:25 +0600

    register authentication

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,14 +1,8 @@
 import express from "express";
+import { register } from "../controllers/auth.js";
 const router = express.Router();
 
-// localhost:8800/api/v1
-router.get("/", (req, res) => {
-  res.send("Hello this is auth");
-});
-
 // localhost:8800/api/v1/register
-router.get("/register", (req, res) => {
-  res.send("Hello this is register");
-});
+router.post("/register", register);
 
 export default router;
