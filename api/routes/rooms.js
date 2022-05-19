e914235f41a87b05fb2e173c3e502efe29cfdd8e
Reza <alialreza98@gmail.com>
2022-05-19 11:52:12 +0600

    complete backend

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,4 +1,27 @@
 import express from "express";
+import {
+  createRoom,
+  deleteRoom,
+  getRoom,
+  getRooms,
+  updateRoom,
+} from "../controllers/room.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 const router = express.Router();
+
+router.post("/:hotelId", verifyAdmin, createRoom);
+
+// update
+router.put("/:id", verifyAdmin, updateRoom);
+
+// delete
+router.delete("/:id/:hotelId", verifyAdmin, deleteRoom);
+
+// get
+
+router.get("/:id", getRoom);
+
+// get all
+router.get("/", getRooms);
 
 export default router;
