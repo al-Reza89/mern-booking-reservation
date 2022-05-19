bd9cb2219f10721fa4b0efada3aa60c546ec8054
Reza <alialreza98@gmail.com>
2022-05-20 00:22:15 +0600

    hotel list page and min and max

--- client/src/pages/list/List.jsx
+++ client/src/pages/list/List.jsx
@@ -6,15 +6,25 @@ import { useState } from "react";
 import { format } from "date-fns";
 import { DateRange } from "react-date-range";
 import SearchItem from "../../components/searchItem/SearchItem";
+import useFetch from "../../hooks/useFetch";
 
 const List = () => {
   const location = useLocation();
-  console.log(location);
 
   const [destination, setDestination] = useState(location.state.destination);
   const [date, setDate] = useState(location.state.date);
   const [openDate, setOpenDate] = useState(false);
   const [options, setOptions] = useState(location.state.options);
+  const [min, setMin] = useState(undefined);
+  const [max, setMax] = useState(undefined);
+
+  const { data, loding, error, reFetch } = useFetch(
+    `/hotels?city=${destination}&min=${min || 0}&max=${max || 9999}`
+  );
+
+  const handleClick = () => {
+    reFetch();
+  };
 
   return (
     <div>
@@ -48,13 +58,21 @@ const List = () => {
                 <span className="lsOptionText">
                   Min price <small>per night</small>
                 </span>
-                <input type="number" className="lsOptionInput" />
+                <input
+                  onChange={(e) => setMin(e.target.value)}
+                  type="number"
+                  className="lsOptionInput"
+                />
               </div>
               <div className="lsOptionItem">
                 <span className="lsOptionText">
                   Max price <small>per night</small>
                 </span>
-                <input type="number" className="lsOptionInput" />
+                <input
+                  onChange={(e) => setMax(e.target.value)}
+                  type="number"
+                  className="lsOptionInput"
+                />
               </div>
               <div className="lsOptionItem">
                 <span className="lsOptionText">Adult</span>
@@ -84,15 +102,19 @@ const List = () => {
                 />
               </div>
             </div>
-            <button>Search</button>
+            <button onClick={handleClick}>Search</button>
           </div>
 
           <div className="listResult">
-            <SearchItem />
-            <SearchItem />
-            <SearchItem />
-            <SearchItem />
-            <SearchItem />
+            {loding ? (
+              "Loading please wait"
+            ) : (
+              <>
+                {data.map((item) => (
+                  <SearchItem item={item} key={item._id} />
+                ))}
+              </>
+            )}
           </div>
         </div>
       </div>
