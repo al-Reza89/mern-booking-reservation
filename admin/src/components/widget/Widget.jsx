1a383973517b698e7e7fb230f0788fdba79a1aa2
Reza <alialreza98@gmail.com>
2022-05-24 23:53:15 +0600

    create widgets with create box shadow css generator

---
+++ admin/src/components/widget/Widget.jsx
@@ -0,0 +1,104 @@
+import "./widget.scss";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import MonetizationOnOutlinedIcon from "@mui/icons-material/MonetizationOnOutlined";
+import AccountBalanceWalletOutlinedIcon from "@mui/icons-material/AccountBalanceWalletOutlined";
+
+const Widget = ({ type }) => {
+  let data;
+  const amount = 100;
+  const diff = 20;
+
+  switch (type) {
+    case "user":
+      data = {
+        title: "USERS",
+        isMoney: false,
+        link: "See all users",
+        icon: (
+          <PersonOutlinedIcon
+            className="icon"
+            style={{
+              color: "crimson",
+              backgroundColor: "rgba(185, 106, 185, 0.541)",
+            }}
+          />
+        ),
+      };
+      break;
+    case "order":
+      data = {
+        title: "ORDERS",
+        isMoney: false,
+        link: "See all orders",
+        icon: (
+          <ShoppingCartOutlinedIcon
+            className="icon"
+            style={{
+              color: "green",
+              backgroundColor: "rgba(77, 72, 122, 0.541)",
+            }}
+          />
+        ),
+      };
+      break;
+    case "earnings":
+      data = {
+        title: "EARNINGS",
+        isMoney: true,
+        link: "View net earnings",
+        icon: (
+          <MonetizationOnOutlinedIcon
+            className="icon"
+            style={{
+              color: "green",
+              backgroundColor: "rgba(159, 221, 59, 0.541)",
+            }}
+          />
+        ),
+      };
+      break;
+    case "balance":
+      data = {
+        title: "BALANCE",
+        isMoney: true,
+        link: "See all users",
+        icon: (
+          <AccountBalanceWalletOutlinedIcon
+            className="icon"
+            style={{
+              color: "purple",
+              backgroundColor: "rgba(228, 227, 235, 0.541)",
+            }}
+          />
+        ),
+      };
+      break;
+
+    default:
+      break;
+  }
+
+  return (
+    <div className="widget">
+      <div className="left">
+        <span className="title">{data.title}</span>
+        <span className="counter">
+          {data.isMoney && "$"}
+          {amount}
+        </span>
+        <span className="link"> {data.link} </span>
+      </div>
+      <div className="right">
+        <div className="percentage positive  ">
+          <KeyboardArrowUpIcon />
+          {diff}%
+        </div>
+        {data.icon}
+      </div>
+    </div>
+  );
+};
+
+export default Widget;
--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -1,13 +1,19 @@
 import "./home.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
+import Widget from "../../components/widget/Widget";
 const Home = () => {
   return (
     <div className="home">
       <Sidebar />
       <div className="homeContainer">
         <Navbar />
-        Home Container
+        <div className="widgets">
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earnings" />
+          <Widget type="balance" />
+        </div>
       </div>
     </div>
   );
