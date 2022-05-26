7c30718b3609c3603eeb6207881b534118cd5b91
Reza <alialreza98@gmail.com>
2022-05-27 04:52:50 +0600

    user input

--- admin/src/pages/new/New.jsx
+++ admin/src/pages/new/New.jsx
@@ -1,7 +1,67 @@
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
 import "./new.scss";
+import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
 
 const New = () => {
-  return <div>New</div>;
+  return (
+    <div className="new">
+      <Sidebar />
+      <div className="newContainer">
+        <Navbar />
+        <div className="top">
+          <h1>Add New User</h1>
+        </div>
+        <div className="bottom">
+          <div className="left">
+            <img
+              src="https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+              alt=""
+            />
+          </div>
+          <div className="right">
+            <form>
+              <div className="formInput">
+                <label htmlFor="file">
+                  Image: <DriveFolderUploadOutlinedIcon className="icon" />{" "}
+                </label>
+                <input type="file" id="file" style={{ display: "none" }} />
+              </div>
+              <div className="formInput">
+                <label>Username</label>
+                <input type="text" placeholder="name_name" />
+              </div>
+              <div className="formInput">
+                <label>Name and Surname</label>
+                <input type="text" placeholder="name name" />
+              </div>
+              <div className="formInput">
+                <label>Email</label>
+                <input type="email" placeholder="name@gmail.com" />
+              </div>
+              <div className="formInput">
+                <label>Phone</label>
+                <input type="number" placeholder="+44 44053 8459 049" />
+              </div>
+              <div className="formInput">
+                <label>Password</label>
+                <input type="password" />
+              </div>
+              <div className="formInput">
+                <label>Address</label>
+                <input type="text" placeholder="Your home address" />
+              </div>
+              <div className="formInput">
+                <label>Country</label>
+                <input type="text" placeholder="Your country" />
+              </div>
+              <button>Send</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default New;
